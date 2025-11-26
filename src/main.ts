bb8ea98fb7e4fa79127c0dad0ba7c5fe6c83e670
26760 <@>
2025-11-26 21:05:22 +0800

    加载主题颜色

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import {createPinia} from "pinia";
 import router from "./router";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import './assets/themes/css/light.css'
+import useSetThemeColor from "./hooks/useSetThemeColor.ts";
+// import './assets/themes/css/light.css'
 
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.use(router);
+await useSetThemeColor();
 app.mount("#app");
 
